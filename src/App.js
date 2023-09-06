aae3ad3b958f8b2d932cc4131a7f04347aa40b9a
IqraSarwarArabi <141229997+IqraSarwarArabi@users.noreply.github.com>
2023-09-06 15:13:47 +0500

    updating footer nav bar placement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AllBlogsPage, HomePage, WritePage, BlogPage } from "./pages/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/write/:bid",
+    element: <WritePage />,
+  },
+  {
+    path: "/all",
+    element: <AllBlogsPage />,
+  },
+  {
+    path: "/blog/:id",
+    element: <BlogPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
